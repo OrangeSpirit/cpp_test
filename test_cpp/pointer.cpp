4b3373df2c715b1ce5d08d16625b0d2817c05bf9
HP_3500 <sergeymaksimov78@yandex.ru>
2024-07-14 14:38:35 +0700

    clear

--- test_cpp/pointer.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-	int a = 111;
-	std::cout << &a << '\n';
-	int* p = &a;
-	std::cout << p << '\n';
-}
-
--- test_cpp/sizeof.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-	int x = 1;
-	bool b = true;
-	std::cout << sizeof(x) << '\n';
-	std::cout << sizeof(b) << '\n';
-}
-
